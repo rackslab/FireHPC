rackslab/FireHPC

commit fa99a457013be3fdc779ae0d7007ce3c80c29058

feat: integrated management of venv for ansible
Add integrated management of virtual environment to multiple versions of
Ansible depending on targeted OS.

For this feature, some changes are introduced:

- the cluster states are moved in clusters subfolder of user state
  folder.
- os and environment parameters are added in cluster settings.
- cluster settings file is now required to run configuration and restore
  as the name of the environment must be retrieved to run ansible.
- images.yml file is renamed os/db.yml and it now assocites the name of
  an environment to every supported OS.
- [images] sections is replaced by [os] section in system configuration
  file.
- The OSImagesSources class is replaced by more generic OSDatabase
  class.

fix #24

firehpc/os.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (C) 2023 Rackslab
+# Copyright (C) 2023-2025 Rackslab
 #
 # This file is part of FireHPC.
 #
@@ -17,6 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with FireHPC.  If not, see <https://www.gnu.org/licenses/>.
 
+"""Manage OS YAML database."""
+
 from __future__ import annotations
 from typing import TYPE_CHECKING
 import logging
@@ -29,20 +31,23 @@ if TYPE_CHECKING:
 logger = logging.getLogger(__name__)
 
 
-class OSImagesSources:
-    def __init__(self, settings: RuntimeSettings) -> OSImagesSources:
-        logger.debug("Loading OS images sources file %s", settings.images.sources)
-        with open(settings.images.sources) as fh:
-            self.sources = yaml.safe_load(fh)
+class OSDatabase:
+    def __init__(self, settings: RuntimeSettings) -> OSDatabase:
+        logger.debug("Loading OS database file %s", settings.os.db)
+        with open(settings.os.db) as fh:
+            self.content = yaml.safe_load(fh)
 
     def supported(self, os: str) -> bool:
-        return os in self.sources.keys()
+        return os in self.content.keys()
 
     def url(self, os: str) -> str:
-        return self.sources[os]
+        return self.content[os]["url"]
+
+    def environment(self, os: str) -> str:
+        return self.content[os]["environment"]
 
     def __str__(self):
         result = ""
-        for os, url in self.sources.items():
-            result += f"{os:10s}: {url}\n"
+        for os, value in self.db.items():
+            result += f"{os:10s}: {value['url']}\n"
         return result

etc/os/db.yml
@@ -1,0 +1,16 @@
+---
+debian11:
+  url: https://hpck.it/osi/firehpc/main/node-debian11_1.raw.xz
+  environment: ansible-latest
+debian12:
+  url: https://hpck.it/osi/firehpc/main/node-debian12_1.raw.xz
+  environment: ansible-latest
+debian13:
+  url: https://hpck.it/osi/firehpc/main/node-debian13_1.raw.xz
+  environment: ansible-latest
+rocky8:
+  url: https://hpck.it/osi/firehpc/main/node-rocky8_1.raw.xz
+  environment: ansible-2.16
+rocky9:
+  url: https://hpck.it/osi/firehpc/main/node-rocky9_1.raw.xz
+  environment: ansible-latest

etc/vendor/firehpc.ini
@@ -3,5 +3,6 @@
 path = /usr/share/firehpc/conf
 args = --diff
 
-[images]
-sources = /usr/share/firehpc/images.yml
+[os]
+db = /usr/share/firehpc/os/db.yml
+requirements = /usr/share/firehpc/os/requirements

etc/vendor/images.yml
@@ -1,6 +1,0 @@
----
-debian11: https://hpck.it/osi/firehpc/main/node-debian11_1.raw.xz
-debian12: https://hpck.it/osi/firehpc/main/node-debian12_1.raw.xz
-debian13: https://hpck.it/osi/firehpc/main/node-debian13_1.raw.xz
-rocky8: https://hpck.it/osi/firehpc/main/node-rocky8_1.raw.xz
-rocky9: https://hpck.it/osi/firehpc/main/node-rocky9_1.raw.xz

firehpc/cluster.py
@@ -22,6 +22,7 @@ from dataclasses import dataclass
 from typing import TYPE_CHECKING, Optional
 from pathlib import Path
 import shutil
+import os
 import logging
 
 import ansible_runner
@@ -32,12 +33,12 @@ from .users import UsersDirectory
 from .containers import ContainersManager
 from .errors import FireHPCRuntimeError
 from .settings import ClusterSettings
-from .state import ClusterState
+from .state import ClusterState, UserState
+from .environments import DeploymentEnvironment
 
 if TYPE_CHECKING:
     from racksdb import RacksDB
     from .settings import RuntimeSettings
-    from .images import OSImagesSources
     from .containers import Container
 
 logger = logging.getLogger(__name__)
@@ -45,7 +46,7 @@ logger = logging.getLogger(__name__)
 
 def clusters_list(state: Path):
     """Return list of cluster names present in state directory."""
-    return [path.name for path in state.glob("*")]
+    return [path.name for path in UserState(state).clusters.glob("*")]
 
 
 @dataclass
@@ -69,7 +70,7 @@ class EmulatedCluster:
         runtime_settings: RuntimeSettings,
         name: str,
         state: ClusterState,
-        cluster_settings: ClusterSettings,
+        cluster_settings: Optional[ClusterSettings] = None,
     ):
         self.runtime_settings = runtime_settings
         self.name = name
@@ -93,7 +94,7 @@ class EmulatedCluster:
     def deploy(
         self,
         os: str,
-        images: OSImagesSources,
+        url: str,
         db: RacksDB,
     ) -> None:
 
@@ -104,7 +105,7 @@ class EmulatedCluster:
         admin_node = infrastructure.nodes.filter(tags=["admin"]).first()
         admin_image = manager.download(
             admin_node.name,
-            images.url(os),
+            url,
         )
         if not self.cluster_settings.slurm_emulator:
             for node in infrastructure.nodes:
@@ -242,7 +243,26 @@ class EmulatedCluster:
         if skip_tags is not None and len(skip_tags):
             cmdline += f" --skip-tags {','.join(skip_tags)}"
 
+        environment = DeploymentEnvironment(
+            self.state.user_state,
+            self.runtime_settings,
+            self.cluster_settings.environment,
+        )
+
+        if not environment.exists():
+            raise FireHPCRuntimeError(
+                f"Unable to find environment {environment.name}, bootstrap first?"
+            )
+
         for playbook in playbooks:
+            # Prepend deployment environment bin folder in $PATH so that
+            # ansible-runnner will execute ansible-playbook in that folder instead of
+            # the one in system paths.
+            logger.debug("Adding %s in PATH", environment.bin)
+            old_path = os.environ["PATH"]
+            os.environ["PATH"] = f"{environment.bin}:{old_path}"
+
+            # Run ansible-playbook
             ansible_runner.run(
                 private_data_dir=self.state.conf,
                 playbook=f"{self.runtime_settings.ansible.path}/{playbook}.yml",
@@ -254,6 +274,9 @@ class EmulatedCluster:
                     "fhpc_nodes": nodes,
                 },
             )
+
+            # Restore $PATH
+            os.environ["PATH"] = old_path
 
         for generated_dir in ["artifacts", "env"]:
             generated_path = self.state.conf / generated_dir

firehpc/dumpers/console.py
@@ -51,6 +51,8 @@ class ClusterSettingsConsoleDumper:
         def label(name):
             return f"  {name:15s}"
 
+        result += f"{label('os')}: {obj.os}\n"
+        result += f"{label('environment')}: {obj.environment}\n"
         if obj.custom:
             result += f"{label('custom')}: {obj.custom}\n"
         result += (

firehpc/environments.py
@@ -1,0 +1,62 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2025 Rackslab
+#
+# This file is part of FireHPC.
+#
+# FireHPC is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# FireHPC is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with FireHPC.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+Manage deployment environments.
+
+This is a mean to have multiple version of Ansible, depending on the targeted operating
+system. The concept is very similar to Ansible Execution Environments (aka. Ansible EE),
+but with simple Python virtual environment rather than podman/docker containers.
+
+This specific implementation is preferred because ansible-builder as this tool is not
+widely available in host distributions supported by FireHPC and requires itself a Python
+virtual environment to be installed. It has been chosen to manage mutltiple virtual
+environments with ansible rather than virtual environment with ansible-builder +
+podman/docker containers. This may change in the future when ansible-builder is more
+widely available in host distributions supported by FireHPC.
+"""
+
+from __future__ import annotations
+import dataclasses
+import typing as t
+import logging
+
+if t.TYPE_CHECKING:
+    from .settings import RuntimeSettings
+    from .state import UserState
+
+logger = logging.getLogger(__name__)
+
+
+@dataclasses.dataclass
+class DeploymentEnvironment:
+    state: UserState
+    runtime_settings: RuntimeSettings
+    name: str
+
+    @property
+    def path(self):
+        return self.state.path / "venvs" / self.name
+
+    @property
+    def bin(self):
+        return self.path / "bin"
+
+    def exists(self):
+        return self.path.exists()

firehpc/exec.py
@@ -31,7 +31,7 @@ from .state import default_state_dir, UserState, ClusterState
 from .cluster import EmulatedCluster, clusters_list
 from .ssh import SSHClient
 from .errors import FireHPCRuntimeError
-from .images import OSImagesSources
+from .os import OSDatabase
 from .load import load_clusters
 from .log import TTYFormatter
 from .dumpers import DumperFactory
@@ -334,8 +334,8 @@ class FireHPCExec:
 
     def _execute_deploy(self):
         # Load images sources
-        images = OSImagesSources(self.runtime_settings)
-        if not images.supported(self.args.os):
+        os_db = OSDatabase(self.runtime_settings)
+        if not os_db.supported(self.args.os):
             logger.critical("OS %s is not supported", self.args.os)
             logger.info("Run `firehpc images` to get the list of supported OS")
             sys.exit(1)
@@ -343,7 +343,14 @@ class FireHPCExec:
         # Define cluster settings from args and save
         state = ClusterState(self.user_state, self.args.cluster)
         state.create()
-        cluster_settings = ClusterSettings.from_args(self.args)
+        cluster_settings = ClusterSettings.from_values(
+            os=self.args.os,
+            environment=os_db.environment(self.args.os),
+            slurm_emulator=self.args.slurm_emulator,
+            db=self.args.db,
+            schema=self.args.schema,
+            custom=self.args.custom,
+        )
         state.save(cluster_settings)
 
         # Load RacksDB
@@ -368,7 +375,7 @@ class FireHPCExec:
             ).users_directory
 
         # Deploy cluster
-        cluster.deploy(self.args.os, images, db)
+        cluster.deploy(self.args.os, os_db.url(self.args.os), db)
         cluster.conf(
             db,
             playbooks=["bootstrap", "site"],
@@ -455,11 +462,8 @@ class FireHPCExec:
     def _execute_clean(self):
         # Load cluster settings
         state = ClusterState(self.user_state, self.args.cluster)
-        cluster_settings = state.load()
 
-        cluster = EmulatedCluster(
-            self.runtime_settings, self.args.cluster, state, cluster_settings
-        )
+        cluster = EmulatedCluster(self.runtime_settings, self.args.cluster, state)
         cluster.clean()
 
     def _execute_status(self):
@@ -477,8 +481,8 @@ class FireHPCExec:
         )
 
     def _execute_images(self):
-        images = OSImagesSources(self.runtime_settings)
-        print(str(images), end="")
+        os_db = OSDatabase(self.runtime_settings)
+        print(str(os_db), end="")
 
     def _execute_list(self):
         print("\n".join(clusters_list(self.args.state)))

commit 5ee246dcbbb6c9e4af6f9574815ab7a1b580c10c

fix(core): wrong attribute
Regression introduced in fa99a45.

firehpc/os.py
@@ -35,6 +35,6 @@ class OSDatabase:
 
     def __str__(self):
         result = ""
-        for os, value in self.db.items():
+        for os, value in self.content.items():
             result += f"{os:10s}: {value['url']}\n"
         return result